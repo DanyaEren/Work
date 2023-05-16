4b0bb8123a559fd3ac789216e9ede8685a1ff719
Danya Erenburg <erenburg.danya2003@mail.ru>
2023-05-16 20:00:29 +0300

    Добавлены некоторые комментарии

--- Zadacha1/Zadacha1/PartB.cs
+++ Zadacha1/Zadacha1/PartB.cs
@@ -11,24 +11,25 @@ namespace Zadacha1
         public static LinkedList<int> Fibonacchi(int n)
         {
             var answer = new LinkedList<int>();
-
+            //проверка, что пользователь выбрал число меньше одного
             if(n < 1)
             {
                 return answer;
             }    
-
+            
             answer.Add(0);
-
+            //проверка, что пользователь выбрал число равное одному
             if(n == 1)
             {
                 return answer;
             }
             
             answer.Add(1);
-
+            // a и b первоначальные числа Фибоначчи
             int a = 0;
             int b = 1;
 
+            //Цикл для создания чисел Фибоначчи с добавлением чисел в список по очереди.
             for(int i =1; i < n; i++)
             {
                 int c = a + b;
--- Zadacha1/Zadacha1/PartC.cs
+++ Zadacha1/Zadacha1/PartC.cs
@@ -8,16 +8,18 @@ namespace Zadacha1
 {
     internal class PartC
     {
+        //Консольное приложение для вывода чисел Фибоначчи
         class Program
         {
             static void Main()
             {
                 Console.WriteLine("Введите количество цифр Фибоначчи(Число n)");
                 int n = int.Parse(Console.ReadLine());
-
+                //Вызов метода для подсчёта чисел Фибоначчи
                 var result = PartB.Fibonacchi(n);
 
                 Console.WriteLine("Числа Фибоначчи");
+                //Вывод чисел Фибоначчи по списку и их вывод
                 foreach (var item in result) Console.WriteLine(item + " ");
 
                 Console.ReadKey();
