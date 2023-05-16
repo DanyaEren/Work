100b7cd6036e8a1f3313f4facfe5f4758b4ad77e
Danya Erenburg <erenburg.danya2003@mail.ru>
2023-05-16 21:22:03 +0300

    Доделано

--- Task2/Task2/Program.cs
+++ Task2/Task2/Program.cs
@@ -24,9 +24,58 @@ namespace Task2
             for(int i = 0; i < files.Length; i++)
             {
                 FileInfo file = files[i];
-                double size = file.Length / 1024 * 1024;
-                Console.WriteLine($"{i + 1}. {file.FullName}, ({size}) MB ");
+                //Округление размера файла до сотых
+                double size = Math.Round((double)file.Length / (1024 * 1024), 2);
+                Console.WriteLine($"| Номер по списку {i + 1}| Полное имя файла {file.FullName}| Размер файла {size} MB|");
             }
+
+            //Сохранение данных в файл
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Хотите ли вы сохранить данные о файлах? Введите ответ 'Да'/'Нет' ");
+
+            //Запоминание ответа и перевод в нижний регистр
+            string answer = Console.ReadLine().ToLower();
+
+            if (answer.Equals("да"))
+            {
+                Console.WriteLine("Введите путь сохранения файла");
+                string savePath = Console.ReadLine();
+                SaveInfoFile(files, savePath);
+            }
+        }
+
+        static void SaveInfoFile(FileInfo[] files, string savePath)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(savePath))
+                {
+                    writer.WriteLine("Список файлов по размеру файлов");
+
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        FileInfo file = files[i];
+                        //Округление размера файла до сотых
+                        double size = Math.Round((double)file.Length / (1024 * 1024), 2);
+                        writer.WriteLine($"| Номер по списку {i + 1}| Полное имя файла {file.FullName}| Размер файла {size} MB|");
+                    }
+                }
+
+                Console.WriteLine($"Файл сохранён по пути '{savePath}'");
+            }
+            catch 
+            {
+                Console.WriteLine("Ошибка сохранение файла.Хотите попробовать снова? Введите ответ 'Да'/'Нет'");
+                string retry = Console.ReadLine().ToLower();
+                if (retry.Equals("да"))
+                {
+                    Console.WriteLine("Введите путь сохранения файла");
+                    savePath = Console.ReadLine();
+                    SaveInfoFile(files, savePath);
+                }
+            }
+
         }
     }
 
