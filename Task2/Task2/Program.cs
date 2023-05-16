c23f40635782fe56945157a44425e8ebfbb52fb2
Danya Erenburg <erenburg.danya2003@mail.ru>
2023-05-16 20:29:31 +0300

    Сделан, вывод файлов из директории(Сделать сохранение в файл)

--- Task2/Task2/Program.cs
+++ Task2/Task2/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Linq;
+namespace Task2
+{
+    class Program
+    { 
+        static void Main()
+        {
+            Console.WriteLine("Введите путь к папке");
+            string path = Console.ReadLine();
+
+            //Проверка, что такой путь действителен с помощью Directory.Exists
+            if (Directory.Exists(path) == false)
+            {
+                Console.WriteLine($" '{path}' Такого пути нет в системе");
+                return; 
+            }
+            //Запоминание информации о директории, с сортировкой по размеру файла, с преобразованием в массив
+            FileInfo[] files = new DirectoryInfo(path).GetFiles("*.*", SearchOption.AllDirectories).OrderByDescending(f => f.Length).ToArray();
+           
+            Console.WriteLine("Вывод списка файлов");
+
+            for(int i = 0; i < files.Length; i++)
+            {
+                FileInfo file = files[i];
+                double size = file.Length / 1024 * 1024;
+                Console.WriteLine($"{i + 1}. {file.FullName}, ({size:F2}) MB ");
+            }
+        }
+    }
+
+}
